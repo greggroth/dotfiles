e9543c76875adcdd4cc2162d5e3602a2175f4495
Greggory Rothmeier <greggroth@gmail.com>
2022-03-15 13:34:33 -0700

    Modernize fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,31 +14,17 @@ alias f "open ."
 alias be "bundle exec"
 
 # Vim
-alias vim "mvim -v"
-
+alias vim "nvim"
 set EDITOR vim
 
 # Homewbrew
 set PATH /usr/local/bin $PATH
 set PATH $PATH /usr/local/sbin
 
-# Julia
-set PATH $PATH /Applications/Julia-0.3.0.app/Contents/Resources/julia/bin
-
 # Scripts
 set PATH $PATH $HOME/bin
 
-# Postgres.app
-set PATH $PATH /Applications/Postgres.app/Contents/Versions/9.3/bin
-set PGHOST localhost
-
 # Chruby
-source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
-
-# Autojump
-source /usr/local/Cellar/autojump/HEAD/etc/autojump.fish
-
-# GoLang
-set GOPATH ~/code/gocode
+source /opt/homebrew/opt/chruby-fish/share/chruby/chruby.fish
+source /opt/homebrew/opt/chruby-fish/share/chruby/auto.fish
 
