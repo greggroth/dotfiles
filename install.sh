0ec7e63dc2495297db118a88e4f5acfe23193b39
Greggory Rothmeier <greggroth@gmail.com>
2022-05-10 14:19:57 -0700

    Lean on chezmoi more

--- install.sh
+++ install.sh
@@ -11,21 +11,6 @@ fi
 echo "Adding git aliases"
 git config --global --add include.path "/workspaces/.codespaces/.persistedshare/dotfiles/git/.gitalias"
 
-# Extra packages I find useful
-sudo apt install -y \
-  tmux \
-  fd-find \
-  ripgrep \
-  fzf \
-  stow
-
-# fd (find replacement)
-# https://github.com/sharkdp/fd
-if ! command -v fd &> /dev/null; then
-  [ ! -d "$HOME/.local/bin" ] && mkdir -p $HOME/.local/bin
-  ln -s $(which fdfind) $HOME/.local/bin/fd
-fi
-
 # git-delta for pretty diffs
 # https://dandavison.github.io/delta
 if ! command -v delta &> /dev/null; then
@@ -35,26 +20,6 @@ if ! command -v delta &> /dev/null; then
   rm git-delta_0.12.1_amd64.deb
   git config --global --add include.path "/workspaces/.codespaces/.persistedshare/dotfiles/git/.gitdelta"
 fi
-
-
-# NeoVim Setup
-echo "Adding NVIM dotfiles"
-stow nvim -t $HOME
-
-if ! command -v nvim  &> /dev/null; then
-  echo "Installing NVIM"
-  wget https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
-  sudo dpkg -i nvim-linux64.deb
-  rm nvim-linux64.deb
-  pip3 install neovim
-  nvim --headless +UpdateRemotePlugins +qa
-fi
-
-# TMUX Setup
-echo "Adding Tmux dotfiles"
-stow tmux -t $HOME
-git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-$HOME/.tmux/plugins/tpm/bin/install_plugins
 
 # Bash Setup
 echo "Adding Bash dotfiles"
