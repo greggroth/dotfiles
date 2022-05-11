b27227692eb60421bfd1a6fd46474540da8c3fc3
Greggory Rothmeier <greggroth@gmail.com>
2022-05-11 13:20:14 -0700

    Move git config to chezmoi

--- install.sh
+++ install.sh
@@ -21,16 +21,3 @@ fi
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # exec: replace current process with chezmoi init
 exec "$chezmoi" init --apply "--source=$script_dir"
-
-echo "Adding git aliases"
-git config --global --add include.path "/workspaces/.codespaces/.persistedshare/dotfiles/git/.gitalias"
-
-# git-delta for pretty diffs
-# https://dandavison.github.io/delta
-if ! command -v delta &> /dev/null; then
-  echo "Installing Git-Delta"
-  wget https://github.com/dandavison/delta/releases/download/0.12.1/git-delta_0.12.1_amd64.deb
-  sudo dpkg -i git-delta_0.12.1_amd64.deb
-  rm git-delta_0.12.1_amd64.deb
-  git config --global --add include.path "/workspaces/.codespaces/.persistedshare/dotfiles/git/.gitdelta"
-fi
